63a42f13c2aebecc17e8f52d744e0fc885ea18f5
Piero Vasquez <piero.vasquez.r@gmail.com>
2021-01-08 18:03:00 +0100

    a short explain on what does the before and after functions inside describe function

--- src/app/app-routing.module.spec.ts
+++ src/app/app-routing.module.spec.ts
@@ -2,8 +2,30 @@ import { routes } from './app-routing.module';
 import { PinsComponent } from './components/pins/pins.component';
 
 fdescribe('App Routing', () => {
+
+  beforeAll(() => {
+    // it executes before all the unit tests
+    console.log('beforeAll');
+  });
+  beforeEach(() => {
+    // it executes before each unit test
+    console.log('beforeEach');
+  });
+  afterAll(() => {
+    // it executes after all unit tests
+    console.log('afterAll');
+  });
+  afterEach(() => {
+    // it executes after each unit test.
+    console.log('afterEach');
+  });
+
   it('should have app as path', () => {
     expect(routes[0].path).toBe('app');
+
+  });
+
+  it('should match the app first children', () => {
     expect(routes[0].children).toContain({
       path: 'pins',
       component: PinsComponent
