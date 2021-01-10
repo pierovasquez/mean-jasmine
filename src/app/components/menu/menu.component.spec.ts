a73be0879d75a33c9314fde9e7739f502477d18f
Piero Vasquez <piero.vasquez.r@gmail.com>
2021-01-10 18:13:44 +0100

    html file tested for the menu component

--- src/app/components/menu/menu.component.spec.ts
+++ src/app/components/menu/menu.component.spec.ts
@@ -1,8 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { MenuComponent } from './menu.component';
 
-describe('MenuComponent', () => {
+fdescribe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
@@ -20,6 +21,7 @@ describe('MenuComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    const title = fixture.debugElement.query(By.css('h1'));
+    expect(title.nativeElement.innerHTML).toBe('eLearning Management System');
   });
 });
