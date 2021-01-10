681e6f5f33bb51579972a253351f9591ef9b30b5
Piero Vasquez <piero.vasquez.r@gmail.com>
2021-01-10 18:05:06 +0100

    deleteAsset and savePins function tested

--- src/app/components/form/form.component.spec.ts
+++ src/app/components/form/form.component.spec.ts
@@ -84,4 +84,28 @@ fdescribe('FormComponent', () => {
       expect(Object.keys(assets.controls)).toEqual(['0', '1']);
     });
   });
+
+  describe('when deleteAsset', () => {
+    it('should remove the form control', () => {
+      const assets = <FormArray>component.secondFormGroup.get('assets');
+
+      component.addAsset();
+      component.deleteAsset(0);
+
+      expect(Object.keys(assets.controls)).toEqual([]);
+    });
+  });
+
+  describe('when savePins is executed', () => {
+    it('should navigate to pins view', () => {
+      const navigate = spyOn((<any>component).navigate, 'goToPins');
+      const open = spyOn((<any>component).snackBar, 'open').and.callThrough();
+
+      component.savePin();
+      expect(navigate).toHaveBeenCalled();
+      expect(open).toHaveBeenCalledWith('Your pin is saved, Redirecting ...', 'Cool!', {
+        duration: 2000
+      });
+    });
+  });
 });
