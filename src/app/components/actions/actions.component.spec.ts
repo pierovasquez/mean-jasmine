c4e911ebd303cfea4611b42d42f79f7fee35f086
Piero Vasquez <piero.vasquez.r@gmail.com>
2021-01-10 19:04:52 +0100

    action component openLink function tested

--- src/app/components/actions/actions.component.spec.ts
+++ src/app/components/actions/actions.component.spec.ts
@@ -1,16 +1,32 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatBottomSheetRef } from '@angular/material';
+import { PinsService } from '../pins/pins.service';
 
 import { ActionsComponent } from './actions.component';
 
-describe('ActionsComponent', () => {
+export class MatBottomSheetRefSTUB {
+  dismiss() { }
+}
+
+export class PinsServiceSTUB {
+  resolveActionObserver(action) { }
+}
+
+fdescribe('ActionsComponent', () => {
   let component: ActionsComponent;
   let fixture: ComponentFixture<ActionsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ActionsComponent ]
+      declarations: [ActionsComponent],
+      providers: [
+        { provide: MatBottomSheetRef, useClass: MatBottomSheetRefSTUB },
+        { provide: PinsService, useClass: PinsServiceSTUB },
+      ],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +37,19 @@ describe('ActionsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should openLink', () => {
+    const event = new MouseEvent('click');
+    const eventCall = spyOn(event, 'preventDefault').and.callFake(() => { });
+    const dismiss = spyOn((<any>component).bottomSheetRef, 'dismiss');
+    const resolveActionObserver = spyOn(
+      (<any>component).pinsService,
+      'resolveActionObserver'
+    );
+    component.openLink(event, '');
+    expect(eventCall).toHaveBeenCalled();
+    expect(dismiss).toHaveBeenCalled();
+    expect(resolveActionObserver).toHaveBeenCalled();
   });
 });
