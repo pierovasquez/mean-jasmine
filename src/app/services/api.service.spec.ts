3145acf412772321f6dde0e18627c09d9fec99a2
Piero Vasquez <piero.vasquez.r@gmail.com>
2021-01-13 23:00:27 +0100

    post, put and delete methods tested

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -2,6 +2,7 @@ import { getTestBed, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ApiService } from './api.service';
 import { environment } from 'src/environments/environment';
+import { HttpHeaders } from '@angular/common/http';
 
 fdescribe('ApiService', () => {
   let service: ApiService;
@@ -38,9 +39,57 @@ fdescribe('ApiService', () => {
       });
 
       const req = httpMock.expectOne(environment.apiEndpoint + '/test');
-      console.log(req);
       expect(req.request.method).toBe('GET');
       req.flush(result);
+    });
+
+    it('should execute GET with headerz', () => {
+      const result = 'testing';
+      const headers = new HttpHeaders().set('platzi-headers', 'cristian-rules');
+      service.get('/test', headers).subscribe(response => {
+        expect(response).toBe(result);
+      });
+
+      const req = httpMock.expectOne(environment.apiEndpoint + '/test');
+      expect(req.request.headers.get('platzi-headers')).toBe('cristian-rules');
+      expect(req.request.method).toBe('GET');
+      req.flush(result);
+    });
+  });
+
+  describe('POST', () => {
+    it('should execute POST', () => {
+      const result = 'testing';
+      service.post('/test', {}).subscribe(response => {
+        expect(response).toBe(result);
+      });
+
+      const req = httpMock.expectOne(environment.apiEndpoint + '/test');
+      expect(req.request.method).toBe('POST');
+      req.flush(result);
+    });
+  });
+  describe('PUT', () => {
+    it('should execute PUT', () => {
+
+      service.put('/testing', {}).subscribe(res => {
+        expect(res).toBeTruthy();
+      });
+
+      const req = httpMock.expectOne(environment.apiEndpoint + '/testing')
+      expect(req.request.method).toBe('PUT');
+    });
+  });
+
+  describe('DELETE', () => {
+    it('should execute DELETE', () => {
+
+      service.delete('/testing', {}).subscribe(res => {
+        expect(res).toBeTruthy();
+      });
+
+      const req = httpMock.expectOne(environment.apiEndpoint + '/testing')
+      expect(req.request.method).toBe('DELETE');
     });
   });
 });
